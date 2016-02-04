7a60d2786fa9116616f180b5662c77cb34185441
Nick Hirakawa <nh0815@gmail.com>
2016-02-03 21:04:30 -0500

    read cpu stat

--- src/main.go
+++ src/main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
-//"bufio"
-//"fmt"
-//"os"
+	//"bufio"
+	//"fmt"
+	"io/ioutil"
+	"strings"
+	//"os"
 )
 
 func main() {
-	println("hello world")
+	//println("hello world")
+	cpu := read_cpu_info()
+	//println(strings.Split(cpu, "\n"))
+	for _, element := range strings.Split(cpu, "\n") {
+		println(element)
+		println()
+	}
+}
+
+func read_cpu_info() string {
+	data, err := ioutil.ReadFile("/proc/stat")
+	if err != nil {
+		panic(err)
+	}
+	return string(data)
 }
