20a4a804af93e68c8eb74aa045d5f8b11c3e0570
Nick Hirakawa <nh0815@gmail.com>
2016-02-03 21:39:25 -0500

    add static file server

--- src/main.go
+++ src/main.go
@@ -4,6 +4,8 @@ import (
 	//"bufio"
 	//"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"strings"
 	//"os"
 )
@@ -13,11 +15,17 @@ func main() {
 	//info := read_cpu_info()
 	//info := read_mem_info()
 	//info := read_disk_info()
-	info := read_net_info()
-	for _, element := range split_on_newline(info) {
-		println(element)
-		println()
-	}
+	/*
+		info := read_net_info()
+		for _, element := range split_on_newline(info) {
+			println(element)
+			println()
+		}
+	*/
+	fs := http.FileServer(http.Dir("../static"))
+	http.Handle("/", fs)
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", nil)
 }
 
 func read_cpu_info() string {
