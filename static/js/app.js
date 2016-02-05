0f6a9cdc738f853806c92b19e694d5ea3c0409f1
Nick Hirakawa <nh0815@gmail.com>
2016-02-04 23:00:25 -0500

    add angular skeleton (no logic changed)

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,32 @@
 
-var socket = new SockJS(window.location.origin + '/ws');
+var app = angular.module('Golem', []);
 
-socket.onopen = function(){
-	console.log('connection open');
-};
+app.controller('GolemController', ['$scope', 'Socket',
+	function($scope, Socket){
+		
+	}
+]);
 
-socket.onmessage = function(e){
-	var systemInfo = JSON.parse(e.data)
-	console.log(systemInfo);
-};
+app.service('Socket', [
+	function(){
+		
+		var socket = new SockJS(window.location.origin + '/ws');
 
-socket.onclose = function(){
-	console.log('connection closed');
-}
+		socket.onopen = function(){
+			console.log('connection open');
+		};
+
+		socket.onmessage = function(e){
+			var systemInfo = JSON.parse(e.data)
+			console.log(systemInfo);
+		};
+
+		socket.onclose = function(){
+			console.log('connection closed');
+		};
+
+		return {};	
+	}
+]);
+
+
