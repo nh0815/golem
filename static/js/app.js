0f93f11b4a0745e09e80b911d82fe70ab49dcf75
Nick Hirakawa <nh0815@gmail.com>
2016-02-04 23:15:59 -0500

    show stats in browser

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,28 @@
 
 var app = angular.module('Golem', []);
 
-app.controller('GolemController', ['$scope', 'Socket',
-	function($scope, Socket){
-		
-	}
-]);
+app.controller('GolemController', ['$scope',
+	function($scope){
 
-app.service('Socket', [
-	function(){
-		
+		$scope.cpu = {
+			user: -1,
+			nice: -1,
+			system: -1,
+			idle: -1,
+			iowait: -1
+		};
+
+		$scope.memory = {
+			total: -1,
+			free: -1
+		};
+
+		$scope.network = {
+			name: '',
+			receiveBytes: -1,
+			transmitBytes: -1
+		};
+
 		var socket = new SockJS(window.location.origin + '/ws');
 
 		socket.onopen = function(){
@@ -17,16 +30,15 @@ app.service('Socket', [
 		};
 
 		socket.onmessage = function(e){
-			var systemInfo = JSON.parse(e.data)
-			console.log(systemInfo);
+			var systemInfo = JSON.parse(e.data);
+			$scope.cpu = systemInfo.cpu;
+			$scope.memory = systemInfo.memory;
+			$scope.network = systemInfo.network;
+			$scope.$apply();
 		};
 
 		socket.onclose = function(){
 			console.log('connection closed');
 		};
-
-		return {};	
 	}
 ]);
-
-
