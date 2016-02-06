5408efa7198397f6b709ec1677bccdae31632409
Nick Hirakawa <nh0815@gmail.com>
2016-02-06 15:08:57 -0500

    consume chart library

--- static/js/app.js
+++ static/js/app.js
@@ -1,50 +1,65 @@
 
-var app = angular.module('Golem', [
-	'nvd3'
-]);
+var app = angular.module('Golem', ['nvd3']);
 
 app.controller('GolemController', ['$scope',
 	function($scope){
 
-		var counter = 0;
-
 		/* chart code */
 
-		var addCpuData = function(cpu, timestamp){
-			var user = {
-				x: timestamp,
-				y: cpu.user
-			};
-
-			var nice = {
-				x: timestamp,
-				y: cpu.nice
-			};
-
-			var system = {
-				x: timestamp,
-				y: cpu.system
-			};
-			$scope.cpuChart.data[0].values.push(user);
-			$scope.cpuChart.data[1].values.push(nice);
-			$scope.cpuChart.data[2].values.push(system);
+		$scope.cpu = {
+			options: {
+				chart: {
+					type: 'lineChart',
+					height: 450,
+				},
+				title: {
+					enable: true,
+					text: 'cpu'
+				}
+			},
+			data: [
+				{
+					values: [
+						{x:0, y:0},
+						{x:1, y:1},
+						{x:2,y:2}
+					],
+					key: '0-5'
+				}
+			]
 		};
 
-		var addMemoryData = function(memory, timestamp){
-			var usage = {
-				x: timestamp,
-				y: 100
-			};
-			$scope.memoryChart.data[0].values.push(usage);
+		$scope.memory = {
+			options: {
+				chart: {
+					type: 'lineChart',
+					height: 450
+				},
+				title: {
+					enable: true,
+					text: 'memory'
+				}
+			},
+			data: [
+				{
+					values: [
+						{x: 100, y:-100},
+						{x: 50, y:60},
+						{x: -10, y:99}
+					],
+					key: 'numbers',
+					area: true
+				}
+			]
 		};
 
-		var addNetworkData = function(network, timestamp){
-			
-		};
+		var addCpuData = function(cpu, timestamp){};
 
-		var addDiskData = function(data, timestamp){
-			
-		};
+		var addMemoryData = function(memory, timestamp){};
+
+		var addNetworkData = function(network, timestamp){};
+
+		var addDiskData = function(data, timestamp){};
 
 		/* end chart code */
 
@@ -57,11 +72,10 @@ app.controller('GolemController', ['$scope',
 		};
 
 		socket.onmessage = function(e){
-			counter++;
 			var systemInfo = JSON.parse(e.data);
 			var time = Date.parse(systemInfo.timestamp);
 			addCpuData(systemInfo.cpu, time);
-			addMemoryData(systemInfo.memory, counter);
+			addMemoryData(systemInfo.memory, time);
 			$scope.$apply();
 		};
 
