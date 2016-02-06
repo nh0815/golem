4a1b7440d4a64b77df7c3c735f70bd1943e888f6
Nick Hirakawa <nh0815@gmail.com>
2016-02-06 17:41:03 -0500

    add cpu usage chart

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,19 @@ var app = angular.module('Golem', ['nvd3']);
 app.controller('GolemController', ['$scope',
 	function($scope){
 
+		var previousCpu = {
+			user: 0,
+			nice: 0,
+			system: 0,
+			idle: 0,
+			iowait: 0,
+			irq: 0,
+			irqSoft: 0,
+			steal: 0,
+			guest: 0,
+			guestNice: 0
+		};
+
 		/* chart code */
 
 		$scope.cpu = {
@@ -45,8 +58,50 @@ app.controller('GolemController', ['$scope',
 			]
 		};
 
+		$scope.disk = {};
+
+		$scope.network = {
+			options: {
+				chart: {
+					type: 'multiBarChart',
+					height: 450
+				},
+				title: {
+					enable: true,
+					text: 'network'
+				}
+			},
+			data: [
+				{
+					values: [],
+					key: 'transmit'
+				},
+				{
+					values: [],
+					key: 'receive'
+				}
+			]
+		};
+
 		var addCpuData = function(cpu, timestamp){
-			$scope.cpu.data[0].values.push({x:timestamp, y:cpu.user});
+			$scope.cpu.data[0].values.push({x:timestamp, y:cpu});
+		};
+
+		var getCpuUsage = function(cpu){
+			var previousIdle = previousCpu.idle + previousCpu.iowait;
+			var idle = cpu.idle + cpu.iowait;
+
+			var previousNonIdle = previousCpu.user + previousCpu.nice + previousCpu.system + previousCpu.irq + previousCpu.irqSoft + previousCpu.steal;
+			var nonIdle = cpu.user + cpu.nice + cpu.system + cpu.irq + cpu.irqSoft + cpu.steal;
+
+			var previousTotal = previousIdle + previousNonIdle;
+			var total = idle + nonIdle;
+
+			var totalDelta = total - previousTotal;
+			var idleDelta = idle - previousIdle;
+
+			return (totalDelta - idleDelta) / totalDelta;
+
 		};
 
 		var addMemoryData = function(memory, timestamp){
@@ -54,7 +109,10 @@ app.controller('GolemController', ['$scope',
 			$scope.memory.data[0].values.push({x: timestamp, y: memoryUsage});
 		};
 
-		var addNetworkData = function(network, timestamp){};
+		var addNetworkData = function(network, timestamp){
+			$scope.network.data[0].values.push({x:timestamp, y:network.transmitBytes});
+			$scope.network.data[1].values.push({x:timestamp, y:network.receiveBytes});
+		};
 
 		var addDiskData = function(data, timestamp){};
 
@@ -71,8 +129,9 @@ app.controller('GolemController', ['$scope',
 		socket.onmessage = function(e){
 			var systemInfo = JSON.parse(e.data);
 			var time = Date.parse(systemInfo.timestamp);
-			addCpuData(systemInfo.cpu, time);
+			addCpuData(getCpuUsage(systemInfo.cpu), time);
 			addMemoryData(systemInfo.memory, time);
+			addNetworkData(systemInfo.network.interfaces[0], time);
 			$scope.$apply();
 		};
 
